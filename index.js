e83ccec6d0e507abd91188c805aaa6566e833f04
Elliot Nash <elliot@venturemedias.com>
2016-05-12 02:38:48 -0400

    enable style as a prop

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ var nodeEmoji = require('node-emoji');
 class Emoji extends React.Component {
   render() {
     var emoji = nodeEmoji.get(this.props.name);
-    return (<React.Text>{emoji}</React.Text>);
+    return (<React.Text style={this.props.style}>{emoji}</React.Text>);
   }
 }
 
 Emoji.propTypes = {
   // Emoji's name
   name: React.PropTypes.string.isRequired,
+  style: React.PropTypes.any
 };
 
 module.exports=Emoji;
